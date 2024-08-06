5fe349999df6f9c5ad4c8a113702d1c75adf0aca
shreyasnaidu2612 <127318151+shreyasnaidu2612@users.noreply.github.com>
2024-08-06 16:51:50 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -61,5 +61,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is runnint on port ${PORT}`));
